cf192539c6f41c642801e87f3406da7aa331bc1b
toshi <me@toshimaru.net>
2016-03-27 14:06:19 +0900

    No parallax effect

--- js/main.js
+++ js/main.js
@@ -13,19 +13,6 @@
          device_type = 'phone';
      }
   }
-
-  $(function(){
-    $('section[data-type="background"]').each(function(){
-      var $bgobj = $(this);
-
-      $(window).scroll(function() {
-        var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
-        var coords = '50% '+ yPos + 'px';
-        $bgobj.css({ backgroundPosition: coords });
-      });
-    });
-  });
-  
 })();
 
 
